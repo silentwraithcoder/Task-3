0735533d09d3cf8012d817a43f24c1bf9f3121f4
silentwraithcoder <72653390+silentwraithcoder@users.noreply.github.com>
2021-08-12 14:17:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,37 +1,44 @@
-# this is Server Page 
-# import library
-import cv2
-import socket
-import pickle
-import struct
-# create Socket 
-try:
-    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket successfully created ...")
-except socket.error as err:
-    print("Socket creation failed with error {}".formatat(err))
-# main Program Block
-# Bind the port no and ipAddres
-port = 1235
-skt.bind(("", port))
-skt.listen()
-print("Socket is listening......")
-try:
-    while True:
-        session, address = skt.accept()
-        print("Connected to : ",address)
-        if session:
-            cam = cv2.VideoCapture(0)
-            while(cam.isOpened()):
-                ret, img = cam.read()
-                data = pickle.dumps(img)
-                msg = struct.pack("Q", len(data))+ data
-                session.sendall(msg)
-                cv2.imshow("Transmitting video...",img)
-                if cv2.waitKey(1) == 13:
-                    cv2.destroyAllWindows()
-                    session.close()
-                    break
-except:
-    cv2.destroyAllWindows()
-    session.close()
+import socket, cv2, pickle,struct
+
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_name  = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print('HOST IP:',host_ip)
+print("\t\t\t\n=================================================")
+port = 9999
+socket_address = ('192.168.56.1',port)
+print("Socket Created")
+print("\t\t\t\n=================================================")
+
+server_socket.bind(socket_address)
+print("Socket Bind Successfully")
+print("\t\t\t\n=================================================")
+
+
+server_socket.listen(5)
+print("LISTENING AT:",socket_address)
+print("\t\t\t\n=================================================")
+
+print("Socket Accept")
+print("\t\t\t\n=================================================")
+while True:
+    client_socket,addr = server_socket.accept()
+    print('GOT CONNECTION FROM:',addr)
+    if client_socket:
+        vid = cv2.VideoCapture(0)
+        
+        while(vid.isOpened()):
+            img,frame = vid.read()
+            a = pickle.dumps(frame)
+            message = struct.pack("Q",len(a))+a
+            client_socket.sendall(message)
+            
+        
+            cv2.imshow('TRANSMITTING VIDEO',frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key ==ord('q'):
+                client_socket.close()
+
+print("Thank you guy's")
+print("\t\t\t\n=================================================")
